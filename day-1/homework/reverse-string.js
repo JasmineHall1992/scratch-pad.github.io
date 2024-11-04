f055dd52d1e8e72ed0ad4ec5ec01e7d75380bcec
JasmineHall1992 <jasminehall5935@gmail.com>
2024-11-03 22:08:44 -0600

    Jazz

--- day-1/homework/reverse-string.js
+++ day-1/homework/reverse-string.js
@@ -18,11 +18,25 @@
  *  c. how do you access individual characters of a String?
  *  d. how do you concatenate Strings? What operator do we use?
  */
-
+//I: string
+//O: a reversed string
+//C: create a loop with a stopping point
+//E: concatenate strings with an operator
 function reverseString(input) {
     // YOUR CODE GOES BELOW HERE //
-    
-    
+    // create an empty variable for the loop
+    var reversed = "";
+    // loop through the string, because it is backwards start at the last item in the string
+    for (let i = input.length - 1; i >= 0; i--) {
+      // access items in string using str[i] and add to variable;
+      //concatenate strings using an operator (+)
+      reversed + str [i];
+    }
+    // return new reversed string
+      return reversed;
+
+    //console log with new function
+    console.log(reverseString('Jasmine'));//'enimsaJ'
     
     
     // YOUR CODE GOES ABOVE HERE //
@@ -38,4 +52,6 @@ if (
 ) {
   // here, export any references you need for tests //
   module.exports.reverseString = reverseString;
+  module.exports.split = split
+  module.exports.join = join
 }
